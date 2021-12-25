8bded56739d4eb42144d5d90e052d0c463721b7d
Eqwe <da_funk_on@mail.ru>
2021-12-25 19:18:24 +0600

    setting the intensity

--- terminal_tree.py
+++ terminal_tree.py
@@ -3,7 +3,8 @@ import os
 import random
 import time
 
-BALL = '⏺'
+
+BALL = '@'
 COLOR = {
     'blue': '\033[94m',
     'yellow': '\033[93m',
@@ -13,7 +14,10 @@ COLOR = {
     'white': '\033[97m',
     'red': '\033[91m'
 }
-STAR = '★'
+STAR = '*'
+intensity_dict = {
+    1: 10, 2: 9, 3: 8, 4: 7, 5: 6, 6: 5, 7: 4, 8: 3, 9: 2, 10: 1
+}
 
 
 def random_change_char(string, value):
@@ -47,9 +51,11 @@ def tree(height=13, screen_width=80):
     return [line.center(screen_width) for line in (*star, *body, trunk)]
 
 
-def balls(tree):
+def balls(tree, intensity):
     for idx, _ in enumerate(tree[:-3], 2):
-        tree[idx] = random_change_char(tree[idx], len(tree[idx])//8)
+        tree[idx] = random_change_char(
+            tree[idx],
+            len(tree[idx])//intensity_dict[intensity])
     return tree
 
 
@@ -60,33 +66,63 @@ def colored_stars_balls(tree):
             if string[pos] == STAR:
                 string[pos] = ''.join([COLOR['yellow'], STAR, '\033[0m'])
             elif string[pos] == BALL:
-                string[pos] = ''.join([random.choice(list(COLOR.values())), BALL, '\033[0m'])
+                string[pos] = ''.join(
+                    [random.choice(list(COLOR.values())), BALL, '\033[0m']
+                )
         tree[idx] = ''.join(string)
     return tree
 
 
 def cli():
-    parser = argparse.ArgumentParser(prog="Python Christmas Tree by Chico Lucio from Ciencia Programada",
-                                     epilog="Ctrl-C interrupts the Christmas :-(")
-    parser.add_argument('-s', '--size', default=13, type=int,
-                        help="Tree height. If even it will be subtracted 1. If less than 7, considered 5. Default: 13")
-    parser.add_argument('-w', '--width', default=80, type=int,
-                        help="Screen width. Used to center the tree. Default: 80")
-    parser.add_argument('-t', '--terminal', action='store_true',
-                        help="Uses the terminal size to center the tree. -s and -w will be ignored")
+    parser = argparse.ArgumentParser(
+        prog="Python Christmas Tree by Chico Lucio from Ciencia Programada",
+        epilog="Ctrl-C interrupts the Christmas :-("
+    )
+    parser.add_argument(
+        '-s', '--size', default=13, type=int,
+        help=(
+            "Tree height. If even it will be subtracted 1. If less than 7,"
+            " considered 5. Default: 13"
+        )
+    )
+    parser.add_argument(
+        '-w', '--width', default=80, type=int,
+        help="Screen width. Used to center the tree. Default: 80"
+    )
+    parser.add_argument(
+        '-t', '--terminal', action='store_true',
+        help=(
+            "Uses the terminal size to center the tree. -s and -w will"
+            " be ignored"
+        )
+    )
+    parser.add_argument(
+        '-i', '--intensity', default=4, type=int,
+        help=(
+            'The number of Christmas tree balls in a row displayed at a time.'
+            ' Max: 10. Min: 1. Default: 4'
+        )
+    )
     args = parser.parse_args()
 
     if args.terminal:
         screen_width, height = os.get_terminal_size()
         height -= 2
+        intensity = args.intensity
     else:
         height = args.size
         screen_width = args.width
+        intensity = args.intensity
     while True:
         try:
             time.sleep(random.uniform(.1, 1))
             os.system('cls' if os.name == 'nt' else 'clear')
-            print('\n'.join(colored_stars_balls(balls(tree(height, screen_width)))))
+            print('\n'.join(
+                colored_stars_balls(
+                    balls(tree(height, screen_width), intensity)
+                    )
+                )
+            )
         except KeyboardInterrupt:
             os.system('cls' if os.name == 'nt' else 'clear')
             print(f"\n{'Merry Christmas!!':^{screen_width}}", end='\n\n')
